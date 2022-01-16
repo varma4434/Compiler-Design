8c6f5e7c882657f55a8e1c86b0d1a9b0c0d9b7e4
nishanth0819 <87228964+nishanth0819@users.noreply.github.com>
2022-01-16 11:05:39 +0530

    Create hello.c

---
+++ exp1/hello.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a ,b , c , d;
+    a = 10;
+    b = 22;
+    c = 19;
+    d = c;
+    if (a > b )
+    {
+        b = b + 2.22;
+    }
+    printf ("%d ",b);
+    return 0;
+}
